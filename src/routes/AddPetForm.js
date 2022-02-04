54b3c43c5d2c96d7dfb43abf100105957164b1b5
Przemysław Kalinowski <przemekk922@gmail.com>
2022-02-04 14:49:17 +0100

    TEL-13 Add diplayer and button to animal list
    Create diplayer of animal list & action on show details button
    
    Co-authored-by: Rafał Welzant <Rafal-Welzant@users.noreply.github.com>
    Co-authored-by: Aleksandra Siwczak <AleksandraSiwczak@users.noreply.github.com>

--- src/routes/AddPetForm.js
+++ src/routes/AddPetForm.js
@@ -1,134 +1,132 @@
 import styled from "styled-components";
 import { useState } from "react";
 import {
-  collection,
-  getDocs,
-  deleteDocs,
-  setDoc,
-  doc,
-  serverTimestamp,
+	collection,
+	getDocs,
+	deleteDocs,
+	setDoc,
+	doc,
+	serverTimestamp,
 } from "firebase/firestore";
 import { db } from "../config";
 import Rating from "@mui/material/Rating";
 import Typography from "@mui/material/Typography";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import { NavLink } from "react-router-dom";
 
 const StyledForm = styled.form`
-  margin: 0 auto;
-  width: 300px;
-  display: flex;
-  flex-direction: column;
+	margin: 0 auto;
+	width: 300px;
+	display: flex;
+	flex-direction: column;
 `;
 
 const StyledRating = styled(Rating)({
-  "& .MuiRating-iconFilled": {
-    color: "#ff6d75",
-  },
-  "& .MuiRating-iconHover": {
-    color: "#ff3d47",
-  },
+	"& .MuiRating-iconFilled": {
+		color: "#ff6d75",
+	},
+	"& .MuiRating-iconHover": {
+		color: "#ff3d47",
+	},
 });
 
 export const AddPetForm = () => {
-  const [animalData, setAnimalData] = useState({
-    id: "",
-    name: "",
-    age: "",
-    species: "",
-    animalBehavior: 0,
-    humanBehavior: 0,
-  });
+	const [animalData, setAnimalData] = useState({
+		id: "",
+		name: "",
+		age: "",
+		species: "",
+		animalBehavior: 0,
+		humanBehavior: 0,
+	});
 
+	const handleChange = (event) => {
+		setAnimalData((prevFormData) => {
+			return {
+				...prevFormData,
+				[event.target.name]: event.target.value,
+			};
+		});
+	};
 
-  const handleChange = (event) => {
-    setAnimalData((prevFormData) => {
-      return {
-        ...prevFormData,
-        [event.target.name]: event.target.value,
-      };
-    });
-  };
+	const addAnimal = async () => {
+		await setDoc(doc(db, "animals", animalData.id), animalData);
+	};
 
-  const addAnimal = async () => {
-    await setDoc(doc(db, "animals", animalData.id), animalData);
-  };
-
-  const handleSumbit = (event) => {
-    event.preventDefault();
-    addAnimal();
-    setAnimalData({
-      id: "",
-      name: "",
-      age: "",
-      species: "",
-	  animalBehavior: 0,
-	  humanBehavior: 0,
-    });
-  };
-  return (
-    <>
-      <StyledForm onSubmit={handleSumbit}>
-        <label htmlFor="id">Id</label>
-        <input
-          id="id"
-          placeholder="Enter pet id"
-          value={animalData.id}
-          type="text"
-          name="id"
-          onChange={handleChange}
-        ></input>
-        <label htmlFor="name">Name</label>
-        <input
-          id="name"
-          placeholder="Enter pet name"
-          value={animalData.name}
-          type="text"
-          name="name"
-          onChange={handleChange}
-        ></input>
-        <label htmlFor="age">Age</label>
-        <input
-          id="age"
-          placeholder="Enter pet age"
-          value={animalData.age}
-          type="text"
-          name="age"
-          onChange={handleChange}
-        ></input>
-        <label htmlFor="species">Species</label>
-        <input
-          name="species"
-          placeholder="Dog/Cat"
-          value={animalData.species}
-          type="text"
-          name="species"
-          onChange={handleChange}
-        ></input>
-        <Typography component="legend">
-          Behavior around other animals
-        </Typography>
-        <StyledRating
-          value={Number(animalData.animalBehavior)}
-          onChange={handleChange}
-          name="animalBehavior"
-          precision={0.5}
-          icon={<FavoriteIcon fontSize="inherit" />}
-          emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
-        />
-        <Typography component="legend">Behavior around humans</Typography>
-        <StyledRating
-          value={Number(animalData.humanBehavior)}
-          onChange={handleChange}
-          name="humanBehavior"
-          precision={0.5}
-          icon={<FavoriteIcon fontSize="inherit" />}
-          emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
-        />
-        <button type="submit">Add Pet</button>
-      </StyledForm>
-
-      <button>Back</button>
-    </>
-  );
+	const handleSumbit = (event) => {
+		event.preventDefault();
+		addAnimal();
+		setAnimalData({
+			id: "",
+			name: "",
+			age: "",
+			species: "",
+			animalBehavior: 0,
+			humanBehavior: 0,
+		});
+	};
+	return (
+		<>
+			<StyledForm onSubmit={handleSumbit}>
+				<label htmlFor="id">Id</label>
+				<input
+					id="id"
+					placeholder="Enter pet id"
+					value={animalData.id}
+					type="text"
+					name="id"
+					onChange={handleChange}
+				></input>
+				<label htmlFor="name">Name</label>
+				<input
+					id="name"
+					placeholder="Enter pet name"
+					value={animalData.name}
+					type="text"
+					name="name"
+					onChange={handleChange}
+				></input>
+				<label htmlFor="age">Age</label>
+				<input
+					id="age"
+					placeholder="Enter pet age"
+					value={animalData.age}
+					type="text"
+					name="age"
+					onChange={handleChange}
+				></input>
+				<label htmlFor="species">Species</label>
+				<input
+					name="species"
+					placeholder="Dog/Cat"
+					value={animalData.species}
+					type="text"
+					name="species"
+					onChange={handleChange}
+				></input>
+				<Typography component="legend">
+					Behavior around other animals
+				</Typography>
+				<StyledRating
+					value={Number(animalData.animalBehavior)}
+					onChange={handleChange}
+					name="animalBehavior"
+					precision={0.5}
+					icon={<FavoriteIcon fontSize="inherit" />}
+					emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
+				/>
+				<Typography component="legend">Behavior around humans</Typography>
+				<StyledRating
+					value={Number(animalData.humanBehavior)}
+					onChange={handleChange}
+					name="humanBehavior"
+					precision={0.5}
+					icon={<FavoriteIcon fontSize="inherit" />}
+					emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
+				/>
+				<button type="submit">Add Pet</button>
+			</StyledForm>
+		</>
+	);
 };
--- src/routes/Navigation.js
+++ src/routes/Navigation.js
@@ -3,72 +3,73 @@ import styled from "styled-components";
 
 import "../styles.css";
 
+const StyledNavigation = styled.div`
+	padding: 150px;
+	background-color: grey;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+	grid-template-rows: 1fr 1fr;
+	grid-gap: 200px;
+	grid-template-areas:
+		"animals" "calendar"
+		"adopted" "addpet";
+`;
+
+const StyledAnimalsBar = styled.div`
+	width: 150px;
+	height: 150px;
+	background-color: black;
+	grid-area: animals;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+const StyledCalendarBar = styled.div`
+	width: 150px;
+	height: 150px;
+	background-color: black;
+	grid-area: calendar;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+const StyledAdoptedBar = styled.div`
+	width: 150px;
+	height: 150px;
+	background-color: black;
+	grid-area: adopted;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+const StyledAddPetBar = styled.div`
+	width: 150px;
+	height: 150px;
+	background-color: black;
+	grid-area: addpet;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
 export const Navigation = () => {
-  const StyledNavigation = styled.div`
-    padding: 150px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-template-rows: 1fr 1fr;
-    grid-gap: 200px;
-    grid-template-areas:
-      "animals" "calendar"
-      "adopted" "addpet";
-  `;
-
-  const StyledAnimalsBar = styled.div`
-    width: 150px;
-    height: 150px;
-    background-color: white;
-    grid-area: animals;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  `;
-  const StyledCalendarBar = styled.div`
-    width: 150px;
-    height: 150px;
-    background-color: white;
-    grid-area: calendar;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  `;
-  const StyledAdoptedBar = styled.div`
-    width: 150px;
-    height: 150px;
-    background-color: white;
-    grid-area: adopted;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  `;
-  const StyledAddPetBar = styled.div`
-    width: 150px;
-    height: 150px;
-    background-color: white;
-    grid-area: addpet;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  `;
-
-  return (
-    <StyledNavigation>
-      <NavLink to="/animalslist">
-        <StyledAnimalsBar>Animals</StyledAnimalsBar>
-      </NavLink>
-      <NavLink to="/calendar">
-        <StyledCalendarBar>Calendar </StyledCalendarBar>
-      </NavLink>
-      <NavLink to="/adoptedanimalslist">
-        <StyledAdoptedBar>Adopted</StyledAdoptedBar>
-      </NavLink>
-      <NavLink to="/adoptedanimalscard">
-        <StyledAddPetBar>Add Pet</StyledAddPetBar>
-      </NavLink>
-    </StyledNavigation>
-  );
+	return (
+		<StyledNavigation>
+			<NavLink to="/animalslist">
+				<StyledAnimalsBar>Animals</StyledAnimalsBar>
+			</NavLink>
+			<NavLink to="/calendar">
+				<StyledCalendarBar>Calendar </StyledCalendarBar>
+			</NavLink>
+			<NavLink to="/adoptedanimalslist">
+				<StyledAdoptedBar>Adopted</StyledAdoptedBar>
+			</NavLink>
+			<NavLink to="/addpet">
+				<StyledAddPetBar>Add Pet</StyledAddPetBar>
+			</NavLink>
+		</StyledNavigation>
+	);
 };
