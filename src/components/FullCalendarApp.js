f6d79f450ae22adb03eec7d16526107c1d268381
Jan Krysztop <jankrysztop@gmail.com>
2022-02-08 09:13:53 +0100

    TEL-17 google callendar added

--- src/components/FullCalendarApp.js
+++ src/components/FullCalendarApp.js
@@ -5,21 +5,6 @@ import interactionPlugin from "@fullcalendar/interaction";
 import styled from "styled-components";
 import { useState, useEffect } from "react";
 
-// const events = [
-//   {
-//     id: 1,
-//     title: "event 1",
-//     start: "2022-02-07T10:00:00",
-//     end: "2021-06-14T12:00:00",
-//   },
-//   {
-//     id: 2,
-//     title: "event 2",
-//     start: "2021-06-16T13:00:00",
-//     end: "2021-06-16T18:00:00",
-//   },
-//   { id: 3, title: "event 3", start: "2021-06-17", end: "2021-06-20" },
-// ];
 
 const StyledCalendar = styled.div`
   width: 80%;
@@ -126,7 +111,10 @@ export const FullCalendarApp = () => {
   return (
     <StyledCalendar>
       <FullCalendar
-        plugins={[dayGridPlugin]}
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        headerToolbar={{
+          center: "dayGridMonth,timeGridWeek,timeGridDay new",
+        }}
         initialView="dayGridMonth"
         events={events}
       />
